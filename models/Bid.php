7e5271cee9e1053c6f179e936a610b54dc506b18
RikiLucy <riki.and.lucy@gmail.com>
2016-07-27 21:20:56 +0700

    Создал модель для таблицы Bid

---
+++ models/Bid.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "bid".
+ *
+ * @property integer $id
+ * @property string $name
+ * @property string $address
+ * @property string $email
+ * @property string $phone
+ * @property string $date
+ */
+class Bid extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'bid';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'address', 'email', 'phone', 'date'], 'required'],
+            [['date'], 'safe'],
+            [['name', 'address', 'email'], 'string', 'max' => 255],
+            [['phone'], 'string', 'max' => 10],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'address' => 'Address',
+            'email' => 'Email',
+            'phone' => 'Phone',
+            'date' => 'Date',
+        ];
+    }
+}
