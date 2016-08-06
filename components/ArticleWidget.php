3acfa74ae434a245e1a0889b20b11b43c592de88
RikiLucy <riki.and.lucy@gmail.com>
2016-08-07 01:13:57 +0700

    Начал писать виджет для отображения статей

--- components/ArticleWidget.php
+++ components/ArticleWidget.php
@@ -7,9 +7,24 @@
  */
 
 namespace app\components;
+use yii\base\Widget;
+use yii\helpers\Html;
+use app\models\articles;
 
 
-class ArticleWidget
+class ArticleWidget extends Widget
 {
 
+    public $articles = [];
+
+    public function init()
+    {
+
+        $articles = articles::find()->all();
+    }
+
+    public function run()
+    {
+
+    }
 }
