fbd7148ededccb5ed8e5aa62b994844052642aa1
RikiLucy <riki.and.lucy@gmail.com>
2016-08-07 20:50:34 +0700

    Добавил виджет и вывел все статьи на главной

--- modules/blog/views/default/index.php
+++ modules/blog/views/default/index.php
@@ -1,10 +1,19 @@
+<?php
+
+
+use yii\helpers\Html;
+use app\components\ArticleWidget;
+//'/' => 'Главная', '/catalog' => 'Каталог', '/catalog/el1' => 'Товар 1'
+$this->title = 'Blog';
+$this->params['breadcrumbs'][] = $this->title;
+?>
 <div class="blog-default-index">
     <h1>Блог со статьями</h1>
-    <p>
-        Тут будут выводиться статьи
-    </p>
-    <p>
-        You may customize this page by editing the following file:<br>
-        <code><?= __FILE__ ?></code>
-    </p>
+
+
+
+    <? foreach ($article as $article): ?>
+    <?= ArticleWidget::widget(['articles' => $article]); ?>
+
+    <? endforeach; ?>
 </div>
