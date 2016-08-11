01dfa0a1c3c5cba289696b9f16c577307c79106a
RikiLucy <riki.and.lucy@gmail.com>
2016-08-11 14:06:36 +0700

    Добавил кэширование запросов

--- modules/blog/controllers/DefaultController.php
+++ modules/blog/controllers/DefaultController.php
@@ -5,7 +5,7 @@ namespace app\modules\blog\controllers;
 use yii\data\Pagination;
 use yii\web\Controller;
 use app\models\articles;
-use yii;
+
 
 
 /**
@@ -20,32 +20,30 @@ class DefaultController extends Controller
     public function behaviors()
     {
         return [
-            'cache' => [
-                'class' => 'yii\filters\PageCache',
-                'only' => ['index'],
-                'variations' => [
-                    Yii::$app->getRequest()->getUrl(),
-                ],
-
-                ],
 
         ];
     }
     public function actionIndex()
     {
-        $query = articles::find()->orderBy('id DESC');
-        $countQuery = clone $query;
-        $pages = new Pagination([
-            'totalCount' => $countQuery->count(),
-            'pageSize' => 2
-    ]);
-        $articles = $query->offset($pages->offset)
-            ->limit($pages->limit)
-            ->all();
-        //$articles = articles::find()->orderBy('id DESC')->all();
+
+        $data = articles::getDb()->cache(function (){
+            $query = articles::find()->orderBy('id DESC');
+            $countQuery = clone $query;
+            $pages = new Pagination([
+                'totalCount' => $countQuery->count(),
+                'pageSize' => 2
+            ]);
+            return [
+                'articles' => $query->offset($pages->offset)
+                ->limit($pages->limit)
+                ->all(),
+                'pages' => $pages
+            ];
+        });
+
         return $this->render('index', [
-            'articles' => $articles,
-            'pages' => $pages
+            'articles' => $data['articles'],
+            'pages' => $data['pages']
         ]);
     }
 }
